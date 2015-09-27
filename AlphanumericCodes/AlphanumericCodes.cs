119ee7c68d6bce862c7e95f20d8ffa22c2479cb5
David Salgado <davidsb@microsoft.com>
2015-09-26 18:09:48 -0700

    Cleaned the Alphanumeric Code generator file

--- AlphanumericCodes/AlphanumericCodes.cs
+++ AlphanumericCodes/AlphanumericCodes.cs
@@ -10,26 +10,29 @@ namespace AlphanumericCodes
     {
         static void Main(string[] args)
         {
-            //Generates alphanumeric codes. Doesn't check for duplicates, I usually do that afterwards with excel :)
+            //Generates alphanumeric codes. Doesn't check for duplicates, I usually do that afterwards with excel.
 
             const string StringWithAllowedCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            char[] ArrayOfAllowedCharacters = StringWithAllowedCharacters.ToCharArray();
+            var ArrayOfAllowedCharacters = StringWithAllowedCharacters.ToCharArray();
 
-            const int CodeSize = 6;         // size of the code... 6 characters
-            const int AmountOfCodes = 20;  //Number of codes to generate
-            char[] CodeBuffer = new Char[CodeSize]; //this will be filled with characters to create the code                                         
+            // Size of the code... 6 characters.
+            const int CodeSize = 6;
+            // Number of codes to generate 
+            const int AmountOfCodes = 20;
+            // This will be filled with characters to create the code.
+            var CodeBuffer = new Char[CodeSize];
 
             var index = 0;
-            var random = new Random();
+            var randomNumber = new Random();
 
             Console.WriteLine("Using the range '{0}', to generate {1} codes of {2} digits each", StringWithAllowedCharacters, AmountOfCodes, CodeSize);
 
-            for (int i = 1; i <= AmountOfCodes; i++)
+            for (var i = 1; i <= AmountOfCodes; i++)
             {
-                for (int CharIndex = 0; CharIndex <= CodeSize - 1; CharIndex++) //for each of the 6 digits
+                for (var CharIndex = 0; CharIndex <= CodeSize - 1; CharIndex++)
                 {
-                    index = random.Next(0, StringWithAllowedCharacters.Length - 1);
-                    CodeBuffer[CharIndex] = ArrayOfAllowedCharacters[index]; //chose a random character from the source
+                    index = randomNumber.Next(0, ArrayOfAllowedCharacters.Length - 1);
+                    CodeBuffer[CharIndex] = ArrayOfAllowedCharacters[index];
                 }
                 Console.WriteLine(new string(CodeBuffer));
             }
