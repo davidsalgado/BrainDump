7f9f4c29b1abe016b37b143b6460d1ba520e0ab4
David Salgado <dasalga@gmail.com>
2016-11-29 18:18:11 -0800

    up

---
+++ WebScrapPY/scrap.py
@@ -0,0 +1,32 @@
+import re
+from urllib.error import URLError, HTTPError
+from http.client import IncompleteRead
+from urllib.request import Request, urlopen
+
+in_file = open('in.txt','r')
+out_file = open('out.txt','w+')
+err_file = open('err.txt','w+')
+
+count  = 1
+
+url_scrap_all = in_file.readlines()
+for url_scrap in url_scrap_all: 
+	print("Processing %s %d" % (url_scrap, count))
+	count = count+1
+	req = Request(url_scrap)
+	req.add_header('User-Agent','Chrome')
+	try:
+		html = urlopen(req).read() #.decode('utf-8')
+	except:
+		print ("error!")
+		err_file.write("%s" % url_scrap)
+	else:
+		emails = re.findall(b'[\w\.-]+@[\w\.-]+', html) 
+		for email in emails:
+		    print (email)
+		    out_file.write("%s\r\n" % email)		
+
+print('et voila')
+out_file.close()
+in_file.close();
+err_file.close();
